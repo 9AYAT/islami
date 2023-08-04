7a1e660b3200831c93d7b740039fff20af161475
User <ayatabdelrhman514@gmail.com>
2023-08-04 03:38:10 +0300

    app init

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  static const String routeName="hhh";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar:AppBar(
+        centerTitle: true,
+
+
+
+
+        title: Text("Islami"),
+      ),
+
+    );
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:untitled6/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    initialRoute:HomeLayout.routeName ,
+      routes:{
+      HomeLayout.routeName:(context)=>HomeLayout(),
+      },
+      debugShowCheckedModeBanner:false,
+    );
+  }
+}
+
+
